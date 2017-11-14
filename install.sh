de8c12e9e9c608c828b7c620169c48009661cbe8
PafKlapper <stan@wopwop.cc>
2017-11-14 18:35:38 +0100

    up

--- install.sh
+++ install.sh
@@ -40,4 +40,3 @@ systemctl reenable networking.service
 ln -s $(dirname $(realpath "$0"))/windowsUsbBootstrapper.service /etc/systemd/system/windowsUsbBootstrapper.service
 systemctl enable windowsUsbBootstrapper.service
 systemctl disable getty@tty0.service
-#aa
