fa72b523c2897fa33e1fce859d376f9871e52d35
PafKlapper <stan@wopwop.cc>
2017-11-18 00:49:49 +0100

    nope

--- install.sh
+++ install.sh
@@ -26,32 +26,6 @@ grub-mkconfig -o /boot/grub/grub.cfg
 # debian specific!
 apt -y install curl wget wpasupplicant xz-utils pv dmidecode
 
-apt install software-properties-common
-
-# https://github.com/nodesource/distributions#debmanual
-add-apt-repository -y -r ppa:chris-lea/node.js
-rm -f /etc/apt/sources.list.d/chris-lea-node_js-*.list
-rm -f /etc/apt/sources.list.d/chris-lea-node_js-*.list.sav
-
-curl --silent https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
-
-
-# Replace with the branch of Node.js or io.js you want to install: node_0.10, node_0.12, node_4.x, node_5.x, etc...
-VERSION=node_6.x
-# The below command will set this correctly, but if lsb_release isn't available, you can set it manually:
-# - For Debian distributions: wheezey, jessie, sid, etc...
-# - For Ubuntu distributions: trusty, xenial, etc...
-# - For Debian or Ubuntu derived distributions your best option is to use the codename corresponding to the upstream release your distribution is based off. This is an advanced scenario and unsupported if your distribution is not listed as supported per earlier in this README.
-DISTRO="$(lsb_release -s -c)"
-echo "deb https://deb.nodesource.com/$VERSION $DISTRO main" | tee /etc/apt/sources.list.d/nodesource.list
-echo "deb-src https://deb.nodesource.com/$VERSION $DISTRO main" | tee -a /etc/apt/sources.list.d/nodesource.list
-
-apt update
-apt install nodejs npm
-
-cd $installationDirectory/externalModules/wetty
-npm install 
-
 cat<<EOF>/etc/network/interfaces
 auto lo
 iface lo inet loopback
