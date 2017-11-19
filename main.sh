ad8cc2ee51cf5cf9a75fffd0c8bf96d4e331b494
PafKlapper <stan@wopwop.cc>
2017-11-19 16:34:10 +0100

    up

--- main.sh
+++ main.sh
@@ -83,7 +83,7 @@ if [ $isError -gt 0 ]; then
 fi
 }
 
-initConstants && initVars 
+initConstants && initVars && cd $installationDirectory
 
 #clear prev log and run with loging enabled (for html)
 cat /dev/null > $logFile
