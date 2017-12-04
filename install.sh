bff74eea30397f2b6758f7993a00f2be1cdf58fc
PafKlapper <stan@wopwop.cc>
2017-12-04 17:49:10 +0100

    up

--- install.sh
+++ install.sh
@@ -16,7 +16,9 @@ installDrivers()
 	#broadcom
 
 	# add broadcom drivers from apt
-	sed -i '/main/s/$/ non-free contrib/' /etc/apt/sources.list
+	if [ ! "$(grep contrib /etc/apt/sources.list | wc -l)" -gt 2 ] ; then
+		sed -i '/main/s/$/ non-free contrib/' /etc/apt/sources.list
+	fi
 	apt update && apt install broadcom-sta-dkms firmware-brcm80211 firmware-b43-installer firmware-b43legacy-installer
 
 	cp -f $installationDirectory/drivers/broadcom/brcmfmac43430a0-sdio.bin /lib/firmware/brcm/ && cp -f $installationDirectory/drivers/broadcom/brcmfmac43430a0-sdio.txt /lib/firmware/brcm/ || { echo "installation of brcmfmac43430a0 failed!"; exit 1; }
