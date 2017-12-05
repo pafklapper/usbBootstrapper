901e594726a4c898b9fed4a42929b4838c681330
PafKlapper <stan@wopwop.cc>
2017-12-05 22:48:06 +0100

    up

--- install.sh
+++ install.sh
@@ -20,13 +20,13 @@ installDrivers()
 		sed -i '/main/s/$/ non-free contrib/' /etc/apt/sources.list
 	fi
 
-if [ ! -f /etc/apt/apt.conf.d/99defaultrelease ]; then
-	echo "APT::Default-Release "stable";" > /etc/apt/apt.conf.d/99defaultrelease && \
-	grep -ve "^#" /etc/apt/sources.list > /etc/apt/sources.list.d/stable.list && \
-	grep -ve "^#" /etc/apt/sources.list | sed 's/stretch/testing/g'> /etc/apt/sources.list.d/testing.list && \
-	mv /etc/apt/sources.list /etc/apt/sources.list.bak
-fi
-	apt update && apt -t testing -y install broadcom-sta-dkms firmware-brcm80211 firmware-b43-installer firmware-b43legacy-installer
+#if [ ! -f /etc/apt/apt.conf.d/99defaultrelease ]; then
+#	echo "APT::Default-Release "stable";" > /etc/apt/apt.conf.d/99defaultrelease && \
+#	grep -ve "^#" /etc/apt/sources.list > /etc/apt/sources.list.d/stable.list && \
+#	grep -ve "^#" /etc/apt/sources.list | sed 's/stretch/testing/g'> /etc/apt/sources.list.d/testing.list && \
+#	mv /etc/apt/sources.list /etc/apt/sources.list.bak
+#fi
+	apt update && apt -y install broadcom-sta-dkms firmware-brcm80211 firmware-b43-installer firmware-b43legacy-installer
 
 	cp -f $installationDirectory/drivers/broadcom/brcmfmac43430a0-sdio.bin /lib/firmware/brcm/ && cp -f $installationDirectory/drivers/broadcom/brcmfmac43430a0-sdio.txt /lib/firmware/brcm/ || { echo "installation of brcmfmac43430a0 failed!"; read; }
 	
